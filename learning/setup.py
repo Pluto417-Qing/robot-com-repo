902f2af7049a3cd4dd98ca74f4e5cb774c573230
yuanqh23 <yuanqh23@31570856602@qq.com>
2025-04-29 16:15:53 +0800

    update basic module: stand, walk, sensor_msg

--- learning/setup.py
+++ learning/setup.py
@@ -1,25 +1,27 @@
 from setuptools import setup
 
-package_name = 'learning'
+package_name = "learning"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='mi',
-    maintainer_email='yuanqh23@31570856602@qq.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="mi",
+    maintainer_email="mi@todo.todo",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            "stand = learning.stand:main",
+            "sensor_msg = learning.sensor_msg:main",
+            "walk = learning.walk:main",
         ],
     },
 )
